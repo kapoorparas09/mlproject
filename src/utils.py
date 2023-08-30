651857be76c123e3ff857ea621e693e47efee2af
Paras Kapoor <kapoorparas0001@gmail.com>
2023-08-30 12:39:00 +0530

    Prediction Pipeline done

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -16,12 +17,12 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, X_test, y_train, y_test, models,param):
+def evaluate_model(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
@@ -35,18 +36,27 @@ def evaluate_model(X_train, X_test, y_train, y_test, models,param):
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            # model.fit(X_train, y_train)
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
 
         return report
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
